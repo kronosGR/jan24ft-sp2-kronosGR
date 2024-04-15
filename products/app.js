56db9e650462a5b4a2c2cf533eb5131580b0e10c
kronosGR <geo.elgeo@gmail.com>
2024-04-15 18:52:11 +0200

    finished product microservice

--- products/app.js
+++ products/app.js
@@ -22,7 +22,7 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  res.render('notfound', { title: 'Product service' });
+  //res.render('notfound', { title: 'Product service' });
   next(createError(404));
 });
 
--- products/routes/index.js
+++ products/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models/index');
+const createHttpError = require('http-errors');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Product service' });
+});
+
+router.get('/:id', async function (req, res, next) {
+  const id = req.params.id;
+  const product = (
+    await db.sequelize.query(
+      `select p.ProductID , p.Name ,p.Color , p.[Size] , p.Weight , p.ListPrice , p.SellStartDate , m.Name as ModelName,
+      c.Name as CategoryName
+      from SalesLT.Product as p
+      inner join SalesLt.ProductModel as m on p.ProductModelID = m.ProductModelID
+      inner join SalesLT.ProductCategory as c on p.ProductCategoryID  = c.ProductCategoryID  where ProductID='${id}'`
+    )
+  )[0][0];
+  const date = new Date(product.SellStartDate);
+  product.SellStartDate = `${
+    date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+  }/${
+    date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+  }/${date.getFullYear()}`;
+
+  res.render('product', { product: product });
 });
 
 module.exports = router;
