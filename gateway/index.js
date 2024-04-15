f3d9b96afc7a3a2d43d772c6624fb7082c9a02a9
kronosGR <geo.elgeo@gmail.com>
2024-04-15 09:59:38 +0200

    gateway configured

--- gateway/index.js
+++ gateway/index.js
@@ -1,8 +1,16 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+require('dotenv').config();
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.listen(8000, () => {
-  console.log("Gateway is running on Port 8000");
+app.use('/customer', proxy('http://localhost:8001'));
+app.use('/product', proxy('http://localhost:8002'));
+app.use('/management', proxy('http://localhost:8003'));
+
+app.listen(process.env.PORT, () => {
+  console.log('Gateway is running on Port ' + process.env.PORT);
 });
