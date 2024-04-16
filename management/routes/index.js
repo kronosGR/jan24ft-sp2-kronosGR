07109d80d02676404c11619657fb2e832c524190
kronosGR <geo.elgeo@gmail.com>
2024-04-16 10:05:19 +0200

    added queryA

--- management/routes/index.js
+++ management/routes/index.js
@@ -1,93 +1,98 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models/index');
 
 /* GET home page. */
 router.get('/querya', async (req, res, next) => {
-	let queryA = [];
-	res.render('querya', { query: queryA });
+  let queryA = (
+    await db.sequelize.query(
+      'select DISTINCT * from SalesLT.ProductCategory pc where pc.ParentProductCategoryID is NULL ORDER by pc.Name  ;'
+    )
+  )[0];
+  console.log(queryA);
+  res.render('querya', { query: queryA });
 });
 
 router.get('/queryb', async (req, res, next) => {
-	let query = [];
-	res.render('queryb', { query: query });
+  let query = [];
+  res.render('queryb', { query: query });
 });
 
 router.get('/queryc', async (req, res, next) => {
-	let query = [];
-	res.render('queryc', { query: query });
+  let query = [];
+  res.render('queryc', { query: query });
 });
 
 router.get('/queryd', async (req, res, next) => {
-	let query = [{Total:0}];
-	res.render('queryd', { query: query[0] });
+  let query = [{ Total: 0 }];
+  res.render('queryd', { query: query[0] });
 });
 
 router.get('/querye', async (req, res, next) => {
-	let query = [];
-	res.render('querye', { query: query });
+  let query = [];
+  res.render('querye', { query: query });
 });
 
 router.get('/queryf', async (req, res, next) => {
-	let query = [];
-	res.render('queryf', { query: query });
+  let query = [];
+  res.render('queryf', { query: query });
 });
 
 router.get('/queryg', async (req, res, next) => {
-	let query = [];
-	res.render('queryg', { query: query });
+  let query = [];
+  res.render('queryg', { query: query });
 });
 
 router.get('/queryh', async (req, res, next) => {
-	let query = [];
-	res.render('queryh', { query: query });
+  let query = [];
+  res.render('queryh', { query: query });
 });
 
 router.get('/', async (req, res, next) => {
-	let options = [
-		{
-			name: 'Query A',
-			link: 'querya',
-			description: 'Display the table results for Query A',
-		},
-		{
-			name: 'Query B',
-			link: 'queryb',
-			description: 'Display the table results for Query B',
-		},
-		{
-			name: 'Query C',
-			link: 'queryc',
-			description: 'Display the table results for Query C',
-		},
-		{
-			name: 'Query D',
-			link: 'queryd',
-			description: 'Display the table results for Query D',
-		},
-		{
-			name: 'Query E',
-			link: 'querye',
-			description: 'Display the table results for Query E',
-		},
-		{
-			name: 'Query F',
-			link: 'queryf',
-			description: 'Query result for PowerBI visualization',
-		},
-		{
-			name: 'Query G',
-			link: 'queryg',
-			description: 'Query result for PowerBI visualization',
-		},
-		{
-			name: 'Query H',
-			link: 'queryh',
-			description: 'Query result for PowerBI visualization',
-		},
-	];
+  let options = [
+    {
+      name: 'Query A',
+      link: 'manager/querya',
+      description: 'Display the table results for Query A',
+    },
+    {
+      name: 'Query B',
+      link: 'manager/queryb',
+      description: 'Display the table results for Query B',
+    },
+    {
+      name: 'Query C',
+      link: 'manager/queryc',
+      description: 'Display the table results for Query C',
+    },
+    {
+      name: 'Query D',
+      link: 'manager/queryd',
+      description: 'Display the table results for Query D',
+    },
+    {
+      name: 'Query E',
+      link: 'manager/querye',
+      description: 'Display the table results for Query E',
+    },
+    {
+      name: 'Query F',
+      link: 'manager/queryf',
+      description: 'Query result for PowerBI visualization',
+    },
+    {
+      name: 'Query G',
+      link: 'manager/queryg',
+      description: 'Query result for PowerBI visualization',
+    },
+    {
+      name: 'Query H',
+      link: 'manager/queryh',
+      description: 'Query result for PowerBI visualization',
+    },
+  ];
 
-	res.render('index', { options: options });
+  res.render('index', { options: options });
 });
 
 module.exports = router;
-
